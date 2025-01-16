3b955ae747170e302bff5cca0f6570acf39ed68b
summersampson <summersampson57@gmail.com>
2025-01-16 09:36:39 -0700

    adding comments

--- Program.cs
+++ Program.cs
@@ -1,22 +1,26 @@
 ﻿using System;
-using System.ComponentModel.Design;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
+
+        // welcoming the user
         Console.WriteLine("Welcome to the dice throwing simulator!");
         Console.WriteLine("How many dice would you like to roll?");
 
+        // calling the diceRollStimulator
         if (int.TryParse(Console.ReadLine(), out int countOfRolls) && countOfRolls > 0)
         {
             DiceRollSimulator simulator = new DiceRollSimulator();
             int[] results = simulator.SimulateRolls(countOfRolls);
 
-            Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+            // explaining how the results work
+            Console.WriteLine("DICE ROLLING SIMULATION RESULTS:");
             Console.WriteLine("Each '*' represents 1% of the total number of rolls.");
-            Console.WriteLine($"Total number of rolls = {countOfRolls}.\n");
+            Console.WriteLine($"Total number of rolls = {countOfRolls}.");
 
+            // doing the part that shows the stars
             for (int i = 2; i <= 12; i++)
             {
                 int percent = (int)Math.Round((double)results[i] / countOfRolls * 100);
@@ -26,6 +30,7 @@ internal class Program
 
             Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
         }
+        // if their number was invalid
         else {
             Console.WriteLine("Invalid input.");
         }
@@ -34,19 +39,20 @@ internal class Program
 
 class DiceRollSimulator
 {
+    // stimulate rolls
     private Random random = new Random();
     public int[] SimulateRolls(int countOfRolls)
         {
-            int[] results = new int[13];
+            int[] Rollresults = new int[13];
 
         for (int i=0; i < countOfRolls; i++)
         {
             int die1 = random.Next(1, 7);
             int die2 = random.Next(1, 7);
             int total = die1 + die2;
-            results[total]++;
+            Rollresults[total]++;
         }
 
-        return results;
+        return Rollresults;
         }
     }
